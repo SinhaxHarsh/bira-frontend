06cccf62a68753252e05871ae48f922376b45e13
SinhaxHarsh <sinhah270@gmail.com>
2025-12-04 19:28:45 +0530

    Added Something Dont worry

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { initCSRF } from "./services/api";
+
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import Tasks from "./pages/Tasks";
@@ -8,16 +11,16 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import AboutApp from "./pages/AboutApp";
 
 export default function App() {
+  useEffect(() => {
+    initCSRF();  // Fetch CSRF cookie on app start
+  }, []);
+
   return (
     <MainLayout>
       <Routes>
-
         <Route path="/" element={<Home />} />
-
-        {/* ABOUT PAGE (Public) */}
         <Route path="/about-app" element={<AboutApp />} />
 
-        {/* PROTECTED ROUTE */}
         <Route
           path="/tasks"
           element={
@@ -27,10 +30,8 @@ export default function App() {
           }
         />
 
-        {/* PUBLIC PAGES */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
       </Routes>
     </MainLayout>
   );
