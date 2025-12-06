5aa5a0fca585e7ea21a3e8995e51451336532d4b
SinhaxHarsh <sinhah270@gmail.com>
2025-12-06 17:35:11 +0530

    Somethiingggg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
     const hour = new Date().getHours();
 
     if (hour < 12) return "Good Morning";
-    if (hour < 18) return "Good Afternoon";
+    if (hour < 16) return "Good Afternoon";
     return "Good Evening";
   }, []);
 
